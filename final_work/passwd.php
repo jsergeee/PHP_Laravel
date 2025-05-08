1fdf704c991a0265d3edbe3ef4b089b3066f0898
sergey <jserge@yandex.ru>
2025-05-08 13:44:21 +0300

    Удалил ненужные файлы

--- final_work/passwd.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo password_hash('Zrjdtyrj+1', PASSWORD_BCRYPT);
--- final_work/resources/views/admin/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- resources/views/admin/dashboard.blade.php -->
-@extends('layouts.app') <!-- или ваш основной шаблон -->
-
-@section('content')
-<h1>Admin Dashboard</h1>
-<p>Добро пожаловать в панель администратора!</p>
-@endsection
